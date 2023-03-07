4c5f8aedd92852773ec9224107a55ec296f1a6a8
Steve <ycsteven.tsao@gmail.com>
2023-03-08 00:26:23 +0800

    test: todolistPending

---
+++ tests/todoListPending_test.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+import "remix_tests.sol";
+import "../todoListPending.sol";
+
+contract TodoListTest {
+    TodoList todoList;
+
+    function beforeEach() public {
+        todoList = new TodoList();
+    }
+
+    function testAddTodo() public {
+        todoList.addTodo("Buy groceries");
+
+        string[] memory todos = todoList.getAllTodo();
+        string memory expected = "Buy groceries";
+
+        Assert.equal(todos[0], expected, "Should add todo.");
+    }
+
+    function testGetTodo() public {
+        todoList.addTodo("Buy groceries");
+
+        string memory todo = todoList.getTodo(0);
+        string memory expected = "Buy groceries";
+
+        Assert.equal(todo, expected, "Should get todo.");
+    }
+
+    function testDeleteTodo() public {
+        todoList.addTodo("Buy groceries");
+
+        todoList.deleteTodo(0);
+
+        string[] memory todos = todoList.getAllTodo();
+        uint256 expected = 0;
+
+        Assert.equal(todos.length, expected, "Should delete todo.");
+    }
+
+    function testSetCompleted() public {
+        todoList.addTodo("Buy groceries");
+
+        todoList.setCompleted(0);
+
+        string[] memory completed = todoList.getAllCompleted();
+        string[] memory todos = todoList.getAllTodo();
+
+        uint256 expectedCompleted = 1;
+        uint256 expectedTodos = 0;
+
+        Assert.equal(completed.length, expectedCompleted, "Should set todo as completed.");
+        Assert.equal(todos.length, expectedTodos, "Should remove todo from todos.");
+    }
+
+    function testSetPending() public {
+        todoList.addTodo("Buy groceries");
+
+        todoList.setPending(0);
+
+        string[] memory pending = todoList.getAllPending();
+        string[] memory todos = todoList.getAllTodo();
+
+        uint256 expectedPending = 1;
+        uint256 expectedTodos = 0;
+
+        Assert.equal(pending.length, expectedPending, "Should set todo as pending.");
+        Assert.equal(todos.length, expectedTodos, "Should remove todo from todos.");
+    }
+}
